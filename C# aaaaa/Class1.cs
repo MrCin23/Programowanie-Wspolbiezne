cece0ccf9604d3a1884685500f6cc5140a4d7ca8
Piotr Sokolinski <piotrolinek@gmail.com>
2024-03-17 17:10:54 +0100

    test

--- C# aaaaa/Class1.cs
+++ C# aaaaa/Class1.cs
@@ -8,5 +8,15 @@ namespace C__aaaaa
 {
     public class Class1
     {
+        private int a;
+
+        public Class1(int a) 
+        { 
+            this.a = a;
+        }
+
+        public int getA() { return a; }
+
+        public void setA(int a) {  this.a = a; }
     }
 }
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -12,5 +12,14 @@ namespace TestProject1
         {
             Assert.Pass();
         }
+
+        [Test]
+        public void Test2()
+        {
+            Class1 cls = new Class1(1);
+            Assert.Equals(1,cls.getA());
+            cls.setA(2);
+            Assert.Equals(2,cls.getA());
+        }
     }
 }
